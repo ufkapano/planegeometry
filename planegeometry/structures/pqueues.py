9b56924f3dadf15a053088c4b308d9a4267d6fef
Andrzej Kapanowski <andrzej.kapanowski@uj.edu.pl>
2020-04-23 21:23:19 +0200

    pqueues added.

---
+++ planegeometry/structures/pqueues.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+import heapq
+
+class PriorityQueue:
+    """A priority queue without repetitions with O(log n) time operations."""
+
+    def __init__(self):
+        self.heap = []
+        self.item_set = set()   # quick search
+
+    def __str__(self):
+        return str(self.heap)
+
+    def push(self, item):
+        if item not in self.item_set:
+            heapq.heappush(self.heap, item)
+            self.item_set.add(item)
+
+    def pop(self):
+        if not self.empty():
+            item = heapq.heappop(self.heap)
+            self.item_set.remove(item)
+            return item
+        else:
+            raise ValueError("priority queue is empty")
+
+    def empty(self):
+        return len(self.heap) == 0
+
+    def __len__(self):
+        return len(self.heap)
+
+# EOF
---
+++ planegeometry/structures/tests/test_pqueues.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+import unittest
+from planegeometry.structures.pqueues import PriorityQueue
+
+class TestPriorityQueue(unittest.TestCase):
+
+    def setUp(self): pass
+
+    def test_pq(self):
+        pq = PriorityQueue()
+        self.assertTrue(pq.empty())
+        self.assertEqual(len(pq), 0)
+        self.assertEqual(str(pq), "[]")
+        pq.push(3)
+        self.assertFalse(pq.empty())
+        self.assertEqual(len(pq), 1)
+        self.assertEqual(str(pq), "[3]")
+        pq.push(3)   # ignored
+        self.assertEqual(len(pq), 1)
+        pq.push(5)
+        self.assertEqual(len(pq), 2)
+        pq.push(2)
+        self.assertEqual(str(pq), "[2, 5, 3]")   # heap
+        self.assertEqual(len(pq), 3)
+        self.assertEqual(pq.pop(), 2)
+        self.assertEqual(pq.pop(), 3)
+        self.assertEqual(pq.pop(), 5)
+
+    def tearDown(self): pass
+
+if __name__ == "__main__":
+
+    unittest.main()
+
+# EOF
