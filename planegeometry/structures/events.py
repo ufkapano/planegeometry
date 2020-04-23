aa17b6e66411122813aa23a330652ce3777adc8d
Andrzej Kapanowski <andrzej.kapanowski@uj.edu.pl>
2020-04-23 20:05:09 +0200

    Events added.

---
+++ planegeometry/structures/events.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+from functools import total_ordering
+
+@total_ordering
+class Event:
+    """The class defining an event."""
+    LEFT = 0
+    CROSSING = 1
+    RIGHT = 2
+    HORIZONTAL = 3
+    VERTICAL = 4
+
+    def __init__(self, pt, event_type, *sequence):
+        self.pt = pt
+        self.type = event_type
+        if len(sequence) == 1:
+            self.segment = sequence[0]
+        elif len(sequence) == 2:
+            self.segment_above = sequence[0]
+            self.segment_below = sequence[1]
+
+    def __str__(self):
+        return "Event({}, {})".format(self.pt, self.type)
+
+    def __eq__(self, other):
+        """Comparison of events (event1 == event2)."""
+        return self.pt == other.pt
+
+    def __ne__(self, other):
+        """Comparison of events (event1 != event2)."""
+        return self.pt != other.pt
+
+    def __lt__(self, other):
+        """Comparison of events (event1 < event2)."""
+        return (self.pt.x, self.pt.y) < (other.pt.x, other.pt.y)
+
+    def __hash__(self):
+        """Hashable events."""
+        return hash((self.pt.x, self.pt.y, self.type))   # hash based on tuple
---
+++ planegeometry/structures/tests/test_events.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+
+import unittest
+from planegeometry.structures.events import Event
+from planegeometry.structures.points import Point
+from planegeometry.structures.segments import Segment
+
+
+class TestEvents(unittest.TestCase):
+
+    def setUp(self):
+        segment1 = Segment(0, 0, 4, 4)
+        segment2 = Segment(1, 3, 3, 1)
+        self.event1 = Event(segment1.pt1, Event.LEFT, segment1)
+        self.event2 = Event(segment1.pt2, Event.RIGHT, segment1)
+        self.event3 = Event(Point(2, 2), Event.CROSSING, segment2, segment1)
+        segment3 = Segment(0, 0, 0, 1)
+        segment4 = Segment(0, 0, 1, 0)
+        self.event4 = Event(segment3.pt1, Event.VERTICAL, segment3)
+        self.event5 = Event(segment4.pt1, Event.HORIZONTAL, segment4)
+
+    def test_cmp(self):
+        self.assertTrue(self.event1 == self.event4)
+        self.assertFalse(self.event1 == self.event2)
+        self.assertTrue(self.event1 != self.event2)
+        self.assertFalse(self.event1 != self.event4)
+        self.assertTrue(self.event1 < self.event2)
+        self.assertFalse(self.event3 < self.event1)
+        self.assertTrue(self.event1 <= self.event2)
+        self.assertFalse(self.event3 <= self.event1)
+        self.assertTrue(self.event3 > self.event1)
+        self.assertFalse(self.event1 > self.event2)
+        self.assertTrue(self.event3 >= self.event1)
+        self.assertFalse(self.event1 >= self.event3)
+
+    def test_hash(self):
+        aset = set()
+        aset.add(self.event1)
+        aset.add(self.event2)
+        aset.add(self.event2)   # ignored
+        self.assertEqual(len(aset), 2)
+        aset.add(self.event3)
+        aset.add(self.event3)
+        self.assertEqual(len(aset), 3)
+
+    def tearDown(self): pass
+
+if __name__ == "__main__":
+
+    unittest.main()
+
+# EOF
