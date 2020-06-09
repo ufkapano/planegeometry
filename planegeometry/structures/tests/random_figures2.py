1c33ef88a1a6c7aa3b4b9cc77d5615304c8c54b7
Andrzej Kapanowski <andrzej.kapanowski@uj.edu.pl>
2020-06-09 21:14:12 +0200

    Using patches.

--- planegeometry/structures/tests/random_figures2.py
+++ planegeometry/structures/tests/random_figures2.py
@@ -2,33 +2,38 @@
 
 import random
 import matplotlib.pyplot as plt
-import matplotlib.patches as mpatches
 from planegeometry.structures.points import Point
 from planegeometry.structures.segments import Segment
 from planegeometry.structures.rectangles import Rectangle
 from planegeometry.structures.triangles import Triangle
 from planegeometry.structures.circles import Circle
 
-rectangle = Rectangle(0.1, 0.3, 0.9, 0.7)
-triangle = Triangle(0.2, 0.2, 0.8, 0.4, 0.6, 0.8)
+plt.axis([0, 1, 0, 1])
+ax = plt.gca()
 
-for segment in rectangle.itersegments():
+rectangle1 = Rectangle(0.1, 0.3, 0.9, 0.7)
+# rectangle1 = plt.Rectangle((0.1, 0.3), 0.8, 0.4, fill=False, color='b')
+rectangle2 = plt.Rectangle((0.6, 0.1), 0.2, 0.1, fill=False, color='m')
+
+for segment in rectangle1.itersegments():
     x = [segment.pt1.x, segment.pt2.x]
     y = [segment.pt1.y, segment.pt2.y]
     #plt.plot(x, y, 'bo-')
     plt.plot(x, y, 'b.-')
 
-for segment in triangle.itersegments():
+triangle1 = Triangle(0.2, 0.2, 0.8, 0.4, 0.6, 0.8)
+
+for segment in triangle1.itersegments():
     x = [segment.pt1.x, segment.pt2.x]
     y = [segment.pt1.y, segment.pt2.y]
     #plt.plot(x, y, 'ro-')
     plt.plot(x, y, 'r.-')
 
-#circle = Circle(0.4, 0.6, 0.2)
-# A Circle is a subclass of an Artist.
-circle = mpatches.Circle((0.4, 0.6), 0.2, color='g', fill=False)
-#plt.gcf().gca().add_artist(circle)
-plt.gca().add_artist(circle)
+#circle1 = Circle(0.4, 0.6, 0.2)
+circle1 = plt.Circle((0.4, 0.6), 0.2, color='g', fill=False)
+
+ax.add_patch(circle1)
+ax.add_patch(rectangle2)
 
 plt.title("Random figures")
 plt.xlabel("x")
